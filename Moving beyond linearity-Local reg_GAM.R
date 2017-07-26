c81e159a2f20d61e6b498df2126cb31acb1b8dce
Ananda Sampurna Bharadwaj Tadikonda <bharadwaj.ananda2000@gmail.com>
2017-07-26 17:37:38 +0530

    Add files via upload

--- Moving beyond linearity-Local reg_GAM.R
+++ Moving beyond linearity-Local reg_GAM.R
@@ -11,10 +11,10 @@ dim(Wage)
 plot(age ,wage)
 title (" Local Regression ")
 
-fit=loess (wage???age ,span =.2, data=Wage) #span is fraction of points to be used for training. It controls flexibility of non-linear fit
+fit=loess (wage~age ,span =.2, data=Wage) #span is fraction of points to be used for training. It controls flexibility of non-linear fit
 summary(fit)
 
-fit2=loess(wage???age ,span =.5, data=Wage)
+fit2=loess(wage~age ,span =.5, data=Wage)
 summary(fit2)
 
 
@@ -26,6 +26,33 @@ lines(age.grid ,predict (fit2 ,data.frame(age=age.grid)),col =" blue",lwd =2)
 legend ("topright",legend =c("Span =0.2" ,"Span =0.5"),col=c("red"," blue"),lty =1, lwd =2, cex =.8)
 
 #The locfit library can also be used for fitting local regression models in R.
+
+
+#Generalized Additive Models (GAMs)
+
+library(gam)
+
+gam.m3 <- gam(wage~s(year,4)+s(age,5)+education,data=Wage)
+summary(gam.m3)
+
+par(mfrow=c(1,3))
+plot(gam.m3,se=TRUE,col="green")
+
+
+gam.lo <- gam(wage~s(year,4)+lo(age,span=0.7)+education,data=Wage)
+plot(gam.lo,se=TRUE,col="green")
+
+library(akima)
+gam.lo.i <- gam(wage~lo(year,age,span=0.5)+education,data=Wage)
+plot(gam.lo.i,se=TRUE,col="green")
+
+gam.lr <- gam(I(wage>250)~year+s(age,df=5)+education,faimly="binomial",data=Wage) #logistic regression GAM
+par(mfrow=c(1,3))
+plot(gam.lr,se=TRUE,col="green")
+
+
+
+
 
 
 
