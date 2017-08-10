ca69c49726ffa6373563c67b00fc62e5c0444fc2
Ananda Sampurna Bharadwaj Tadikonda <bharadwaj.ananda2000@gmail.com>
2017-08-10 18:22:40 +0530

    Add files via upload

--- web.R
+++ web.R
@@ -10,8 +10,8 @@ library(data.table)
 library(foreach)
 library(doParallel) 
 
-#dir <- "D:\\Users\\bharadwaj\\Desktop\\DS\\Datasets\\Kaggle\\Web Traffic"
-dir <- "C:\\Users\\Srimala Bharadwaj\\Desktop\\Data science\\Kaggle\\Web Traffic"
+dir <- "D:\\Users\\bharadwaj\\Desktop\\DS\\Datasets\\Kaggle\\Web Traffic"
+#dir <- "C:\\Users\\Srimala Bharadwaj\\Desktop\\Data science\\Kaggle\\Web Traffic"
 
 train_1 <- fread("train_1.csv")
 
@@ -25,10 +25,10 @@ train.date.cols = names(train_1[,-1])
 
 # reshape the training data into long format page, date and views
 dt <- melt(train_1,
-           d.vars = c("Page"),
-           measure.vars = train.date.cols,
-           variable.name = "ds",
-           value.name = "y")
+          d.vars = c("Page"),
+          measure.vars = train.date.cols,
+          variable.name = "ds",
+          value.name = "y")
 #######################################################################################################################################
 # replace NAs with 0 and calculate page median
 dt[is.na(y), y :=0]
@@ -59,6 +59,28 @@ fwrite(sub[,c('Id', 'Visits')],file="median_2m.csv")
 
 
 #######################################################################################################################################
+
+dt_sum1 <- dt[,.(Visits = median(y[(length(y)-49):length(y)])) , by = Page]
+setkey(dt_sum1, Page) #dt_sum1 data.table is sorted by Page. Accesses "by reference" and is memory efficient
+
+
+sub <- merge(key_1, dt_sum1, all.x = TRUE) #merge based on shared key columns in key_1 and dt_sum
+
+#write output
+fwrite(sub[,c('Id', 'Visits')],file="median_7w.csv")
+#######################################################################################################################################
+
+dt_sum1 <- dt[,.(Visits = median(y[(length(y)-35):length(y)])) , by = Page]
+setkey(dt_sum1, Page) #dt_sum1 data.table is sorted by Page. Accesses "by reference" and is memory efficient
+
+
+sub <- merge(key_1, dt_sum1, all.x = TRUE) #merge based on shared key columns in key_1 and dt_sum
+
+#write output
+fwrite(sub[,c('Id', 'Visits')],file="median_5w.csv")
+#######################################################################################################################################
+
+
 
 # calculate page mean
 dt_sum1 <- dt[,.(Visits = mean(y)) , by = Page]
@@ -116,106 +138,6 @@ fwrite(sub[,c('Id', 'Visits')],file="mean_out_2m.csv")
 
 #######################################################################################################################################
 
-#setup parallel backend to use many processors
-cores=detectCores()
-cl <- makeCluster(cores[1]) #not to overload your computer
-registerDoParallel(cl)
-
-system.time({
-  
-  sma.model <- foreach(i=1:nrow(train_1), .combine=rbind,.packages="smooth","forecast","data.table") %dopar% {
-    #sma model
-    idx <- which(train_1$Page[i]==key_1$Page)[1]
-    test_op <- data.table(Id=key_1$Id[idx:(idx+59)],Visits=sma(tsclean(ts(as.data.table(t(train_1[i,2:551]))$V1, frequency=365,start=c(2015,182))), h=60,silent="graph")$forecast)
-    test_op
-  }
-})
-
-#stop cluster
-stopCluster(cl)
-#######################################################################################################################################
-
-
-
-#setup parallel backend to use many processors
-cores=detectCores()
-cl <- makeCluster(cores[1]) #not to overload your computer
-registerDoParallel(cl)
-
-
-sma.model <- foreach(i=1:nrow(train_2), .combine=rbind,.packages="smooth","forecast","data.table") %dopar% {
-  #sma model
-  idx <- which(train_2$Page[i]==key_1$Page)[1]
-  test_op <- data.table(Id=key_1$Id[idx:(idx+59)],Visits=sma(tsclean(ts(as.data.table(t(train_2[i,2:551]))$V1, frequency=365,start=c(2015,182))), h=60,silent="graph")$forecast)
-  cat(i,";")
-  test_op
-}
-
-#stop cluster
-stopCluster(cl)
-
-
-
-
-
-
-key_1_upd <- separate(key_1,"Page",c("Page","Date"),sep=-11)
-key_1_upd$Page <- substr(key_1_upd$Page, 1,nchar(key_1_upd$Page)-1) 
-
-
-for(i in 1:nrow(train1))
-{
-  #creating data in proper format
-  t1 <- as.data.frame(t(train_1[i,2:551]))
-  
-  #time series - daily
-  x <- ts(t1$V1, frequency=365,start=c(2015,182))
-  
-  #sma model
-  SMA_test <- sma(tsclean(x), h=60,silent="graph")
-  
-  #building output data
-  idx <- which(train_1$Page[i]==key_1_upd)[1]
-  dir <- "C:\\Users\\Srimala Bharadwaj\\Desktop\\Data science\\AV\\JetRail"
-  test_op <- data.frame(Id=key_1_upd[idx:(idx+59),]$Id,Visits=SMA_test$forecast)
-  colnames(test_op) <- c("Id","Visits")
-  write_csv(test_op, file.path(dir, "smadly.csv"), col_names=ifelse(i %in% 1, TRUE, FALSE), append=TRUE)
-  cat(i,";")
-}
-
-
-
-
-t1 <- as.data.frame(t(train_1[1,2:551]))
-
-train_1[,-1][1, row.names:=train_1$Page]
-
-
-#time series - daily
-x <- ts(t1$V1, frequency=365,start=c(2015,182))
-#sma model
-SMA_test <- sma(tsclean(x), h=60,silent="graph")
-#building output data
-idx <- which(train_1$Page[1]==key_1_upd)[1]
-test_op <- data.frame(Id=key_1_upd[idx:(idx+59),]$Id,Visits=SMA_test$forecast)
-colnames(test_op) <- c("Id","Visits")
-write_csv(test_op, file.path(dir, "smadly.csv"), col_names=ifelse(i %in% 1, TRUE, FALSE), append=TRUE)
-cat(1,";")
-
-
-t1 <- as.data.frame(t(train_1[2,2:551]))
-#time series - daily
-x <- ts(t1$V1, frequency=365,start=c(2015,182))
-#sma model
-SMA_test <- sma(tsclean(x), h=60,silent="graph")
-#building output data
-idx <- which(train_1$Page[2]==key_1_upd)[1]
-test_op <- data.frame(Id=key_1_upd[idx:(idx+59),]$Id,Visits=SMA_test$forecast)
-colnames(test_op) <- c("Id","Visits")
-write_csv(test_op, file.path(dir, "smadly.csv"), col_names=ifelse(i %in% 1, TRUE, FALSE), append=TRUE)
-cat(2,";")
-
-#######################################################################################################################################
 func_out_mean <- function(x, na.rm = TRUE, ...) {
   qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
   H <- 1.5 * IQR(x, na.rm = na.rm)
@@ -226,50 +148,197 @@ func_out_mean <- function(x, na.rm = TRUE, ...) {
 }
 
 
-dt1 <- dt
-dt1[, dow:= as.numeric(format(as.Date(ds),"%w"))]
-
-
 # calculate page mean removing outliers
-dt_sum1 <- dt1[,.(Visits = func_out_mean(y[(length(y)-59):length(y)])) , by = c("Page","dow")]
-setkey(dt_sum1, Page, dow) #dt_sum1 data.table is sorted by Page. Accesses "by reference" and is memory efficient
-
-key_1_new <- fread("key_1.csv")
-#merge projection dates and key to create submission
-key_1_new[, Page2:= substr(Page, 1, nchar(Page)-11)]
-key_1_new[, dow:= as.numeric(format(as.Date(substr(Page, nchar(Page)-9, nchar(Page))),"%w"))]
-key_1_new[, Page:= NULL]
-setnames(key_1_new, "Page2", "Page") #rename Page2 to Page
-setkey(key_1_new, Page, dow)  
+dt_sum1 <- dt[,.(Visits = func_out_mean(sqrt(y[(length(y)-59):length(y)]))**2) , by = Page]
+setkey(dt_sum1, Page) #dt_sum1 data.table is sorted by Page. Accesses "by reference" and is memory efficient
 
 
-
-sub <- merge(key_1_new, dt_sum1, all.x = TRUE) #merge based on shared key columns in key_1 and dt_sum
+sub <- merge(key_1, dt_sum1, all.x = TRUE) #merge based on shared key columns in key_1 and dt_sum
 
 #write output
-fwrite(sub[,c('Id', 'Visits')],file="mean_out_2m_dow.csv")
+fwrite(sub[,c('Id', 'Visits')],file="mean_out_2m_sqrt.csv")
 
 #######################################################################################################################################
-
-func_out_median <- function(x, na.rm = TRUE, ...) {
+i <- 1
+func_out_sma <- function(x, na.rm = TRUE, ...) {
   qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
   H <- 1.5 * IQR(x, na.rm = na.rm)
   y <- x
   y[x < (qnt[1] - H)] <- NA
   y[x > (qnt[2] + H)] <- NA
-  median(y,na.rm=TRUE)
+  #mean(y,na.rm=TRUE)
+  sma(tsclean(ts(na.omit(y))),h=1,silent="graph")$forecast
+  cat(i,";")
+  i <<- i+1 
 }
 
 
 # calculate page mean removing outliers
-dt_sum1 <- dt1[,.(Visits = func_out_median(y[(length(y)-59):length(y)])) , by = c("Page","dow")]
-setkey(dt_sum1, Page, dow) #dt_sum1 data.table is sorted by Page. Accesses "by reference" and is memory efficient
+dt_sum1 <- dt[,.(Visits = func_out_sma(y[(length(y)-59):length(y)])) , by = Page]
+setkey(dt_sum1, Page) #dt_sum1 data.table is sorted by Page. Accesses "by reference" and is memory efficient
 
 
-sub <- merge(key_1_new, dt_sum1, all.x = TRUE) #merge based on shared key columns in key_1 and dt_sum
+sub <- merge(key_1, dt_sum1, all.x = TRUE) #merge based on shared key columns in key_1 and dt_sum
 
 #write output
-fwrite(sub[,c('Id', 'Visits')],file="median_out_2m_dow.csv")
+fwrite(sub[,c('Id', 'Visits')],file="mean_out_2m_sma.csv")
+
 #######################################################################################################################################
+
+#setup parallel backend to use many processors
+cores=detectCores()
+cl <- makeCluster(cores[1]) #not to overload your computer
+registerDoParallel(cl)
+
+strt <- Sys.time()
+
+sma.model <- foreach(i=1:nrow(train_1), .combine=rbind,.packages=c("smooth","forecast","data.table")) %dopar% {
+    #sma model
+    idx <- which(train_1$Page[i]==key_1$Page)[1]
+    test_op <- data.table(Id=key_1$Id[idx:(idx+59)],Visits=sma(tsclean(ts(as.data.table(t(train_1[i,2:551]))$V1, frequency=365,start=c(2015,182))), h=60,silent="graph")$forecast)
+    test_op
+  }
+
+print(Sys.time()-strt)
+#stop cluster
+stopCluster(cl)
+#######################################################################################################################################
+
+#setup parallel backend to use many processors
+cores=detectCores()
+cl <- makeCluster(cores[1]) #not to overload your computer
+#registerDoParallel(cl)
+library(doSNOW)
+registerDoSNOW(cl)
+
+#set up progress bar to track progress
+library(utils)
+pb <- txtProgressBar(max = nrow(train_2), style=3)
+progress <- function(n) setTxtProgressBar(pb, n)
+opts <- list(progress = progress)
+
+#start time of parallel process
+strt <- Sys.time()
+
+sma.model <- foreach(i=1:nrow(train_2), .combine=rbind,.packages=c("smooth","forecast","data.table"),.options.snow = opts) %dopar% {
+  #sma model
+  setTxtProgressBar(pb, i)
+  
+  idx <- which(train_2$Page[i]==key_1$Page)[1]
+  test_op <- data.table(Id=key_1$Id[idx:(idx+59)],Visits=sma(tsclean(ts(as.data.table(t(train_2[i,2:551]))$V1, frequency=365,start=c(2015,182))), h=60,silent="graph")$forecast)
+  #test_op
+  fwrite(test_op,file="temp.csv",append=TRUE)
+}
+
+print(Sys.time()-strt)
+#close progress bar
+close(pb)
+#stop cluster
+stopCluster(cl)
+#######################################################################################################################################
+strt <- Sys.time()
+sma.model <- data.table()
+for(i in 1:nrow(train_1))
+{
+  idx <- which(train_1$Page[i]==key_1$Page)[1]
+  test_op <- data.table(Id=key_1$Id[idx:(idx+59)],Visits=sma(ts(as.data.table(t(train_1[i,2:551]))$V1, frequency=365,start=c(2015,182)), h=60,silent="graph")$forecast)
+  sma.model <- rbind(sma.model,test_op)
+  if(i %% 500 == 0)
+  {
+    print(i)
+  }
+   
+}
+print(Sys.time()-strt)
+colnames(sma.model) <- c('Id', 'Visits')
+fwrite(sma.model,file="sma_model.csv")
+
+#######################################################################################################################################
+
+#setup parallel backend to use many processors
+cores=detectCores()
+cl <- makeCluster(cores[1]) #not to overload your computer
+#registerDoParallel(cl)
+library(doSNOW)
+registerDoSNOW(cl)
+
+#set up progress bar to track progress
+library(utils)
+pb <- txtProgressBar(max = nrow(train_2), style=3)
+progress <- function(n) setTxtProgressBar(pb, n)
+opts <- list(progress = progress)
+
+#start time of parallel process
+strt <- Sys.time()
+
+sma.model <- foreach(s=1:2, .combine=rbind,.packages=c("smooth","forecast","data.table"),.options.snow = opts) %dopar% {
+  #sma model
+  for(i in 1:nrow)
+  setTxtProgressBar(pb, i)
+  
+  idx <- which(train_2$Page[i]==key_1$Page)[1]
+  test_op <- data.table(Id=key_1$Id[idx:(idx+59)],Visits=sma(tsclean(ts(as.data.table(t(train_2[i,2:551]))$V1, frequency=365,start=c(2015,182))), h=60,silent="graph")$forecast)
+  test_op
+}
+
+print(Sys.time()-strt)
+#close progress bar
+close(pb)
+#stop cluster
+stopCluster(cl)
+
+
+
+
+
+
+
+
+foreach(s=1:2, .combine=rbind,.packages=c("smooth","forecast","data.table"),.options.snow = opts) %dopar% {
+  #sma model
+  for(i in 1:nrow)
+    setTxtProgressBar(pb, i)
+  
+  idx <- which(train_2$Page[i]==key_1$Page)[1]
+  test_op <- data.table(Id=key_1$Id[idx:(idx+59)],Visits=sma(tsclean(ts(as.data.table(t(train_2[i,2:551]))$V1, frequency=365,start=c(2015,182))), h=60,silent="graph")$forecast)
+  test_op
+}
+
+
+#########################################################################################################################################
+
+
+med_2m <- fread("median_2m.csv")
+med_7w <- fread("median_7w.csv")
+
+med_2m_7w <- med_2m
+
+med_2m_7w$Visits <- (med_2m$Visits+med_7w$Visits)/2
+
+fwrite(med_2m_7w,file="median_2m_7w_avg.csv")
+
+#########################################################################################################################################
+
+train_1[is.na(train_1)] <- 0
+sma_model <- fread("sma_model.csv")
+
+idx <- 1
+
+strt <- Sys.time()
+
+for(i in 1:nrow(train_1))
+{
+  test_op <- data.table(Id=sma_model$Id[idx:(idx+59)],Visits=forecast(holt(ts(as.data.table(t(train_1[i,2:551]))$V1, frequency=365,start=c(2015,182))),60)$mean)
+  idx <- idx+60
+  fwrite(test_op,file="hw_model.csv",append=TRUE)
+  if(i %% 500 == 0)
+  {
+    print(i)
+  }
+  
+}
+print(Sys.time()-strt)
+#########################################################################################################################################
+
 
 
